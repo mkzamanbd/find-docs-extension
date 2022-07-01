4b3373834ee0be21742dad2b66991158ddcb2003
Kamruzzaman <zamandotbd@gmail.com>
2022-07-01 20:10:06 +0600

    update on load dom run script

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-// translation id
-fetch('https://api.kzaman.me/api/get-notification')
-    .then((response) => response.json())
-    .then((json) => {
-        findDomElement(json.notifications);
-    });
+window.onload = function() {
+    // translation id
+    fetch('https://api.kzaman.me/api/get-notification')
+        .then((response) => response.json())
+        .then((json) => {
+            findDomElement(json.notifications);
+        });
 
-// find dom element with text
-function findDomElement(searches) {
-    const nodeElements = document.querySelectorAll('span');
-    nodeElements.forEach((element) => {
-        const transactionId = element?.childNodes[2]?.textContent?.replace(": ", '');
-        if (transactionId) {
-            searches.forEach((search) => {
-                if (transactionId == search.transaction_id) {
-                    element.children[0].setAttribute('style', 'background: yellow;');
-                    //element.style.backgroundColor = 'yellow';
-                    element.setAttribute('title', search.android_text);
-                    //console.log('Element', element.innerText);
-                }
-            });
-        }
-    });
-}
+    // find dom element with text
+    function findDomElement(searches) {
+        const nodeElements = document.querySelectorAll('span');
+        nodeElements.forEach((element) => {
+            const transactionId = element?.childNodes[1]?.textContent?.replace(": ", '');
+            if (transactionId) {
+                searches.forEach((search) => {
+                    if (transactionId == search.transaction_id) {
+                        element.children[0].setAttribute('style', 'background: yellow;');
+                        //element.style.backgroundColor = 'yellow';
+                        element.setAttribute('title', search.android_text);
+                        //console.log('Element', element.innerText);
+                    }
+                });
+            }
+        });
+    }
+};
