a427480f948f98394bb7c2587de7c33aa1fab24f
Kamruzzaman <zamandotbd@gmail.com>
2022-07-02 16:00:01 +0600

    update extension

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+function findElement() {
     // translation id
     fetch('https://api.kzaman.me/api/get-notification')
         .then((response) => response.json())
@@ -24,3 +24,32 @@ window.onload = function() {
         });
     }
 };
+
+const observeDOM = (function(){
+    const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+    return function( obj, callback ){
+        if( !obj || obj.nodeType !== 1 ) return; 
+
+        if( MutationObserver ){
+            // define a new observer
+            const mutationObserver = new MutationObserver(callback)
+
+            // have the observer observe foo for changes in children
+            mutationObserver.observe( obj, { childList:true, subtree:true })
+            return mutationObserver
+        }
+        
+        // browser support fallback
+        else if( window.addEventListener ){
+            obj.addEventListener('DOMNodeInserted', callback, false)
+            obj.addEventListener('DOMNodeRemoved', callback, false)
+        }
+    }
+})()
+
+const listElm = document.querySelector('.mainpanel');
+// Observe a specific DOM element:
+observeDOM(listElm, function(m){
+    findElement();
+});
